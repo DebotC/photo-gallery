08b065294a91543b7a69a5932ee2bd814530f17f
Cedric <cedric.debot@gmail.com>
2023-11-02 21:57:32 +0100

    feat: saving photos to filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
 
 
 @Injectable({
@@ -18,11 +19,41 @@ export class PhotoService {
       quality: 100
     });
 
-    this.photos.unshift({
-      filePath: 'soon...',
-      webviewPath: caputedPhoto.webPath
-    });
+    const savedPhoto = await this.savePhoto(caputedPhoto);
+    this.photos.unshift(savedPhoto);
   }
+
+  private async savePhoto(photo: Photo): Promise<UserPhoto> {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = `${Date.now()}.jpeg`;
+    const savedFile = Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    return {
+      filePath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo): Promise<string> {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  })
 }
 
 export interface UserPhoto {
